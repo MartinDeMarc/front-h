dc5d6b9a0f90e1f760a5030e81a9562b289fe2ee
Braifz <braian94lp@gmail.com>
2023-04-01 14:09:37 -0300

    create component input-file

---
+++ src/app/components/input-file/input-file.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'InputFile',
+  templateUrl: './input-file.component.html',
+  styleUrls: ['./input-file.component.css'],
+})
+export class InputFile {
+  // Inject service
+  constructor() {}
+}
