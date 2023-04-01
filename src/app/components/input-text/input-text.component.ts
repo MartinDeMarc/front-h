0a1a47497c4ab79fe713fc216f6161e69b3cef93
Braifz <braian94lp@gmail.com>
2023-04-01 14:16:10 -0300

    component input text added

---
+++ src/app/components/input-text/input-text.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'InputText',
+  templateUrl: './input-text.component.html',
+  styleUrls: ['./input-text.component.css'],
+})
+export class InputText {
+  // Inject service
+  constructor() {}
+}
